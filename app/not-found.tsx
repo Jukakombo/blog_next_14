17effca9ccc1ad6507260f910051b805e3c1b286
github username <email@gmail.com>
2024-02-13 23:04:56 +0000

    add again

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h2>Sorry! What you are trying to find doen't exist</h2>
+      <h2>Sorry! What you are trying to find doen&apos;t exist</h2>
       <Link href={"/"}>Return Home</Link>
     </div>
   );
